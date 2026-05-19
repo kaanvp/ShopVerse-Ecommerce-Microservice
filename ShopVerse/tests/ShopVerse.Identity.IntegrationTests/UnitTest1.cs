735aef17a852e93da1e9e969cbd9f84b6ad17696
kaanvuralpolat <vural042010@gmail.com>
2026-05-19 15:30:27 +0300

    Proje mimari iskeletleri oluşturuldu.

---
+++ ShopVerse/tests/ShopVerse.Identity.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ShopVerse.Identity.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ShopVerse/tests/ShopVerse.Order.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ShopVerse.Order.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
